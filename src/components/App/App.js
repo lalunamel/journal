1061fa01848667170c18f53162e49aac377d6a40
Cody Sehl <cody.sehl@gmail.com>
2018-07-30 15:08:32 -0600

    Added entry form

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,13 +3,23 @@ import "./App.css";
 
 import Nav from "../Nav";
 import Header from "../Header";
+import EntryForm from "../EntryForm";
 
 class App extends Component {
+  tagEditorValueChanged(value) {
+    console.log(value);
+  }
+
+  entryEditorValueChanged(value) {
+    console.log(value);
+  }
+
   render() {
     return (
       <div className="App">
         <Nav />
         <Header />
+        <EntryForm entry={""} tags={[]} tagEditorValueChanged={this.tagEditorValueChanged} entryEditorValueChanged={this.entryEditorValueChanged} />
       </div>
     );
   }
---
+++ src/components/EntryForm.js
@@ -0,0 +1,60 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const styles = {
+  entryForm: {
+    margin: "60px auto",
+    width: 700,
+    display: "flex",
+    flexDirection: "column"
+  },
+  entryEditor: {
+    display: "block",
+    width: "100%",
+    height: 180,
+    boxSizing: "border-box",
+    resize: "none"
+  },
+  tagEditor: {
+    marginTop: 20,
+    width: "100%",
+    boxSizing: "border-box"
+  },
+  submitButton: {
+    width: "max-content",
+    alignSelf: "flex-end",
+    marginTop: 20,
+    backgroundColor: "lightgray",
+    border: "none",
+    padding: "3px 10px"
+  }
+};
+
+const EntryForm = props => {
+  const entryEditorOnChange = e => {
+    props.entryEditorValueChanged(e.target.value);
+  };
+
+  const tagEditorOnChange = e => {
+    props.tagEditorValueChanged(e.target.value);
+  };
+
+  return (
+    <div style={styles.entryForm}>
+      <textarea style={styles.entryEditor} onChange={entryEditorOnChange} value={props.entry} placeholder={"Dear diary..."} />
+      <input style={styles.tagEditor} onChange={tagEditorOnChange} type="text" value={props.tags.join(", ")} placeholder={"software, android, cats"} />
+      <button style={styles.submitButton} type="submit">
+        SUBMIT
+      </button>
+    </div>
+  );
+};
+
+EntryForm.propTypes = {
+  entry: PropTypes.string,
+  tags: PropTypes.arrayOf(PropTypes.string),
+  entryEditorValueChanged: PropTypes.func,
+  tagEditorValueChanged: PropTypes.func
+};
+
+export default EntryForm;
